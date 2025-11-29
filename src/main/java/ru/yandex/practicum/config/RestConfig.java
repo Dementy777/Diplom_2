513ffb50326b46774499a405abfb3b242f32efcb
Dementy Sivakov <dsivakov@yandex.ru>
2025-11-30 00:26:37 +0300

    Создан класс RestConfig с константами для ручек

---
+++ src/main/java/ru/yandex/practicum/config/RestConfig.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.config;
+
+public class RestConfig {
+
+    public static final String HOST="https://stellarburgers.education-services.ru";
+    public static final String GETINGREDIENTS="/api/ingredients";
+    public static final String POSTORDERS="/api/orders";
+    public static final String POSTPASSWORDRESET="/api/password-reset";
+    public static final String POSTREGISTER="/api/auth/register";
+    public static final String POSTLOGOUT="/api/auth/logout";
+    public static final String POSTTOKEN="/api/auth/token";
+    public static final String DELETUSER="/api/auth/user";
+}
